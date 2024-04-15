e135c894cbcc51c84c308f11ceca772407623012
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-04-15 19:37:18 -0400

    Update radixsort.cpp

--- radixsort.cpp
+++ radixsort.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int operations_count = 0;
+// interger value that counts the number of operation the sort will do
+
+// for a radix sort, you must first get the max value in the array, THEN sort the numbers based on the unit place.
 int MaxinArr(<vector> arr, int n) {
   int maxVal = arr[0];
   for (int i = 0; i < arr.size(); i++){
